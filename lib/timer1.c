a5b94ff9c38212cea543dda48a5158eb2e380981
Marián <Matiasus@users.noreply.github.com>
2017-12-22 08:06:30 +0100

    Create timer1.c

---
+++ lib/timer1.c
@@ -0,0 +1,14 @@
+
+/** 
+ * @desription  16 bits Timer Counter generator
+ * ---------------------------------------------------
+ * Copyright (C) 2017 Marian Hrinko.
+ * Written by Marian Hrinko (mato.hrinko@gmail.com)
+ *
+ * @author      Marian Hrinko
+ * @datum       22.12.2017
+ * @file        timer.c
+ * @tested      
+ * @inspiration 
+ */
+ 

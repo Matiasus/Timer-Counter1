194dc8ca237d97ffe2d90c40198dc0928234caae
Marián <Matiasus@users.noreply.github.com>
2017-12-22 10:58:54 +0100

    Create timer1.c

---
+++ lib/timer1.c
@@ -0,0 +1,27 @@
+/** 
+ * @desription  16 bits Timer Counter generator
+ * ---------------------------------------------------
+ * Copyright (C) 2017 Marian Hrinko.
+ * Written by Marian Hrinko (mato.hrinko@gmail.com)
+ *
+ * @author      Marian Hrinko
+ * @datum       22.12.2017
+ * @file        timer1.c
+ * @tested      
+ * @inspiration 
+ */
+ 
+#include <stdio.h>
+#include <avr/io.h>
+#include "timer1.h"
+
+/**
+ * @description Required frequency
+ *
+ * @param   long int
+ * @return  int
+ */
+unsigned int req_frequency(long int)
+{
+  
+}

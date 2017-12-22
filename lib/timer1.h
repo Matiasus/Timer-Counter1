9ddd2fe882b4d4a7f3e4d6e67ebc36a575e79f9d
Marián <Matiasus@users.noreply.github.com>
2017-12-22 08:10:38 +0100

    Update and rename timer1.c to timer1.h

--- lib/timer1.h
+++ lib/timer1.h
@@ -11,4 +11,16 @@
  * @tested      
  * @inspiration 
  */
- 
+// define 
+#ifndef _TIMER1_H_
+  #define _TIMER1_H_
+
+// define clock foe Atmega 8
+  #if defined(__AVR_ATMEGA8__)
+    #define _FCPU 8000000
+  // define clock for atmega 16
+  #elif defined(__AVR_ATMEGA16__)
+    #define _FCPU 16000000
+  #endif
+
+#endif
